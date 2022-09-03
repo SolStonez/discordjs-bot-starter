d719aa614ad550a8af7462ce4345372ff5f3c50b
SolStonez <stonez.solana@gmail.com>
2022-09-03 16:21:37 -0700

    add worker

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,6 +22,6 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORDJS_BOT_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, "958409351834529802"), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
